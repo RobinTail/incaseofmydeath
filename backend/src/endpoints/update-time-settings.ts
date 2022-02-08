0ad4a3a33fc0ff2483ce579c50972cb9f5e3bbb4
Robin Tail <robin_tail@me.com>
2022-02-08 22:23:23 +0100

    Fixing death consideration issue before checking alive in case changing time settings (2).

--- backend/src/endpoints/update-time-settings.ts
+++ backend/src/endpoints/update-time-settings.ts
@@ -20,10 +20,12 @@ export const updateTimeSettingsEndpoint = authorizedUserFactory.build({
     if (!user) {
       throw new Error("User not found");
     }
+    // @todo this might better trigger Disposer alive hook
     user.checkFreq = checkFreq;
     user.deadlineDays = deadlineDays;
     user.attemptsCount = attemptsCount;
     user.isAlive = true;
+    user.isCountdown = false;
     user.lastConfirmation = new Date();
     user.nextCheck = new Date(
       Date.now() + checkFreqToDays(checkFreq) * msInDay
