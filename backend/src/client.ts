d68363369637da2590a68630982f7ac96e8bac81
Robin Tail <robin_tail@me.com>
2023-06-22 22:28:21 +0200

    Using express-zod-api v11.

--- backend/src/client.ts
+++ backend/src/client.ts
@@ -1,9 +1,9 @@
-import { Client } from "express-zod-api";
+import { Integration } from "express-zod-api";
 import { routing } from "./routing";
 import fs from "fs";
 
 fs.writeFileSync(
   "../frontend/src/api-client.ts",
-  new Client({ routing }).print(),
+  new Integration({ routing }).print(),
   "utf-8"
 );
