239d9263e04ec0facc719608d1f291d2f3ef6764
Robin Tail <robin_tail@me.com>
2024-08-07 20:44:52 +0200

    rm redundant import.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import globals from "globals";
 import jsPlugin from "@eslint/js";
-import { readFileSync } from "node:fs";
 import tsPlugin from "typescript-eslint";
 import prettierOverrides from "eslint-config-prettier";
 import prettierRules from "eslint-plugin-prettier/recommended";
