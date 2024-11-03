00627c2cb59b5d00710ab633765dff343ec0af7f
Robin Tail <robin_tail@me.com>
2024-11-03 16:39:28 +0100

    Only install production dependencies on server.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -52,7 +52,7 @@ module.exports = {
       ].join(" && "),
       "post-deploy": [
         "source ~/.zshrc",
-        "yarn workspace backend install",
+        "yarn workspace backend install --production --frozen-lockfile",
         "pm2 reload ecosystem.config.cjs",
       ].join(" && "),
     },
