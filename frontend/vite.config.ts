1317af51e6372a043e0ea064644c8a13af33b9b7
Robin Tail <robin_tail@me.com>
2024-11-29 15:15:07 +0100

    Preparing the list of icons in vite config.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const icons = [
+  "account_circle",
+  "brightness_4",
+  "brightness_7",
+  "build_circle",
+  "chat",
+  "comment",
+  "content_copy",
+  "conversion_path",
+  "delete",
+  "fact_check",
+  "home",
+  "info",
+  "play_circle",
+  "ring_volume",
+  "send",
+  "wifi_off",
+].toSorted();
+
+process.env.VITE_ICONS = icons.join(",");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
