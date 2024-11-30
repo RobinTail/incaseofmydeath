3958a678d49c66d1f55fce31f3a993ae77311be1
Robin Tail <robin_tail@me.com>
2024-11-30 15:43:54 +0100

    Using the distributable plugin.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import esquery, { Literal } from "esquery";
-
-const registry = new Set<string>();
+import materialSymbols from "vite-plugin-material-symbols";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "material-symbols",
-      enforce: "pre",
-      moduleParsed: function ({ id, ast }) {
-        const nodes = esquery.query(
-          ast,
-          "CallExpression[callee.name='jsx'][arguments.0.name='Icon'] > .arguments > Property[key.name='children'] Literal",
-        ) as unknown as Literal[];
-        this.debug({ id, message: "value" });
-        for (const { value } of nodes) {
-          if (typeof value === "string") registry.add(value);
-        }
-      },
-      transformIndexHtml: (html) =>
-        html.replace(
-          "__MATERIAL_SYMBOLS__",
-          registry.size
-            ? `icon_names=${Array.from(registry.values()).toSorted().join(",")}`
-            : "", // dev mode, all icons
-        ),
-    },
-  ],
+  plugins: [react(), materialSymbols()],
 });
