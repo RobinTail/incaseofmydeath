0fe5e7a4cf5d95248c2ffa17790dacdb1ca2a390
Robin Tail <robin_tail@me.com>
2024-11-30 14:04:18 +0100

    Extracting material symbols using custom vite plugin.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import esquery, { Literal } from "esquery";
 
-const icons = [
-  "account_circle",
-  "brightness_4",
-  "brightness_7",
-  "build_circle",
-  "chat",
-  "comment",
-  "content_copy",
-  "conversion_path",
-  "delete",
-  "fact_check",
-  "home",
-  "info",
-  "play_circle",
-  "ring_volume",
-  "send",
-  "wifi_off",
-].toSorted();
-
-process.env.VITE_ICONS = icons.join(",");
+const registry = new Set<string>();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "material-symbols",
+      enforce: "pre",
+      moduleParsed: function ({ id, ast }) {
+        const nodes = esquery.query(
+          ast,
+          "CallExpression[callee.name='jsx'][arguments.0.name='Icon'] > .arguments > Property[key.name='children'] Literal",
+        ) as unknown as Literal[];
+        this.debug({ id, message: "value" });
+        for (const { value } of nodes) {
+          if (typeof value === "string") registry.add(value);
+        }
+      },
+      transformIndexHtml: (html) =>
+        html.replace(
+          "__MATERIAL_SYMBOLS__",
+          registry.size
+            ? `icon_names=${Array.from(registry.values()).toSorted().join(",")}`
+            : "", // dev mode, all icons
+        ),
+    },
+  ],
 });
