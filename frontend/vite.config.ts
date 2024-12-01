40f26ef970b4e2900af812dcc171b247b4dfb068
Anna Bocharova <robin_tail@me.com>
2024-12-01 11:19:21 +0100

    Extracting Material Symbols using custom Vite plugin (#1945)
    * Extracting material symbols using custom vite plugin.
    
    * Using the distributable plugin.
    
    * Use 0.1.1.
    
    * 0.2.0.
    
    * No need for ES2023 lib.
    
    * Using Vite 6 and plugin 0.2.1.
    
    * Deduplicating vite.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
-const icons = [
-  "account_circle",
-  "brightness_4",
-  "brightness_7",
-  "build_circle",
-  "chat",
-  "comment",
-  "content_copy",
-  "conversion_path",
-  "delete",
-  "fact_check",
-  "home",
-  "info",
-  "play_circle",
-  "ring_volume",
-  "send",
-  "wifi_off",
-].toSorted();
-
-process.env.VITE_ICONS = icons.join(",");
+import materialSymbols from "vite-plugin-material-symbols";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), materialSymbols()],
 });
