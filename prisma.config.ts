b64caeaec405f0b6d76a2a50d7ff711acda6106a
Robin Tail <robin_tail@me.com>
2026-03-21 21:17:34 +0100

    fix(db): add db url (2).

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,10 @@
 import path from "node:path";
-import { defineConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
 });
